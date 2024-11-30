3814f4aa6000bdca8137ccbc9847c25d1e5331d5
Edan <ewstasiuk03@gmail.com>
2024-11-29 17:02:48 -0800

    Hardcode trained model results, restructure interfaces
    Now displaying results from both models, and only expecting to receive the emotion result from the backend.

--- frontend/src/app/components/app/ClassificationReportDisplay.tsx
+++ frontend/src/app/components/app/ClassificationReportDisplay.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 import Image from "next/image";
-import { ClassificationReport, Metric } from "../../config/interfaces";
+import {
+  DanielClassificationReport,
+  MaishaClassificationReport,
+  DanielMetric,
+  MaishaMetric,
+} from "../../config/interfaces";
 
 interface ClassificationReportDisplayProps {
-  classificationReport: ClassificationReport | null;
+  modelType: string;
+  classificationReport:
+    | DanielClassificationReport
+    | MaishaClassificationReport
+    | null;
   confusionMatrixImage: string | null;
 }
 
 const ClassificationReportDisplay: React.FC<
   ClassificationReportDisplayProps
-> = ({ classificationReport, confusionMatrixImage }) => {
+> = ({ modelType, classificationReport, confusionMatrixImage }) => {
   return (
-    <div className="lg:flex">
+    <div className="flex flex-col items-center">
       {/* Display the classification report */}
       <div className="lg:mr-10">
         {classificationReport && (
           <div className="mt-6 w-full max-w-[700px] overflow-x-auto">
-            <h2 className="text-xl font-semibold mb-4 flex justify-center">
-              Classification Report:
+            <h2 className="text-xl font-semibold mb-4 text-center">
+              {modelType} Model Classification Report:
             </h2>
             <table className="table-auto w-full text-md border-collapse">
               <thead>
@@ -30,38 +39,50 @@ const ClassificationReportDisplay: React.FC<
                 </tr>
               </thead>
               <tbody>
-                {classificationReport.metrics.map((metric: Metric, index) => (
-                  <tr key={index}>
-                    <td className="border-b p-2 text-left">{metric.label}</td>
-                    <td className="border-b p-2 text-left">
-                      {metric.precision}
-                    </td>
-                    <td className="border-b p-2 text-left">{metric.recall}</td>
-                    <td className="border-b p-2 text-left">{metric.f1Score}</td>
-                    <td className="border-b p-2 text-left">{metric.support}</td>
-                  </tr>
-                ))}
+                {classificationReport.metrics.map(
+                  (metric: DanielMetric | MaishaMetric, index) => (
+                    <tr key={index}>
+                      <td className="border-b p-2 text-left">{metric.label}</td>
+                      <td className="border-b p-2 text-left">
+                        {metric.precision}
+                      </td>
+                      <td className="border-b p-2 text-left">
+                        {metric.recall}
+                      </td>
+                      <td className="border-b p-2 text-left">
+                        {metric.f1Score}
+                      </td>
+                      <td className="border-b p-2 text-left">
+                        {metric.support}
+                      </td>
+                    </tr>
+                  )
+                )}
               </tbody>
             </table>
           </div>
         )}
         <div className="mt-4 p-2 text-lg">
           <p className="mb-2">
-            Test Accuracy: {classificationReport?.testAccuracy}%
+            Test Accuracy: {classificationReport?.accuracy}%
           </p>
-          <p>Test Loss: {classificationReport?.testLoss}%</p>
+          {classificationReport?.loss !== undefined && (
+            <p>Test Loss: {classificationReport.loss}%</p>
+          )}
         </div>
       </div>
 
       {/* Display the confusion matrix image */}
       {confusionMatrixImage && (
-        <div className="mt-6">
-          <h2 className="text-xl font-semibold mb-4 flex justify-center">Confusion Matrix:</h2>
+        <div className="mt-6 justify-center">
+          <h2 className="text-xl font-semibold mb-4 text-center">
+            {modelType} Model Confusion Matrix:
+          </h2>
           <Image
             src={confusionMatrixImage}
             alt="Confusion Matrix"
-            width={400}
-            height={400}
+            width={1200}
+            height={900}
           />
         </div>
       )}
--- frontend/src/app/config/interfaces.ts
+++ frontend/src/app/config/interfaces.ts
@@ -1,5 +1,40 @@
+/**
+ * Result passed from backend
+ */
+export interface EmotionResult {
+  emotion: string; // Predicted emotion (e.g., 'Amusement')
+}
+
+/**
+ * Daniel's model's results
+ **/
+
 // Interface for each metric in the classification report
-export interface Metric {
+export interface DanielMetric {
+  label: string; // Label of the class (e.g., 'Anger', 'Joy', 'Sadness')
+  precision: number | null; // Precision value, can be null for certain rows like "Accuracy"
+  recall: number | null; // Recall value, can be null for certain rows like "Accuracy"
+  f1Score: number | null; // F1 Score, can be null for certain rows like "Accuracy"
+  support: number | null; // Support value (number of samples), can be null for certain rows
+}
+
+// Interface for the classification report
+export interface DanielClassificationReport {
+  metrics: DanielMetric[]; // Array of Metric objects
+  accuracy: number; // Overall test accuracy
+  loss: undefined;
+}
+
+// Main structure for test data
+export interface DanielData {
+  classificationReport: DanielClassificationReport; // Classification report with metrics and accuracy
+  confusionMatrixImage: string; // URL or base64 string for the confusion matrix image
+}
+
+/**
+ * Maisha's model's results
+ **/
+export interface MaishaMetric {
   label: string; // Label of the class (e.g., 'Neutral', 'Negative', 'Positive')
   precision: number | null; // Precision value, can be null for certain rows like "Accuracy"
   recall: number | null; // Recall value, can be null for certain rows like "Accuracy"
@@ -8,15 +43,14 @@ export interface Metric {
 }
 
 // Interface for the classification report
-export interface ClassificationReport {
-  metrics: Metric[]; // Array of Metric objects
-  testAccuracy: number; // Overall test accuracy
-  testLoss: number; // Loss value for the test
+export interface MaishaClassificationReport {
+  metrics: MaishaMetric[]; // Array of Metric objects
+  accuracy: number; // Overall test accuracy
+  loss: number; // Loss value for the test
 }
 
 // Main structure for test data
-export interface Data {
-  emotion: string; // Predicted emotion (e.g., 'Amusement')
-  classificationReport: ClassificationReport; // Classification report with metrics and accuracy
+export interface MaishaData {
+  classificationReport: MaishaClassificationReport; // Classification report with metrics and accuracy
   confusionMatrixImage: string; // URL or base64 string for the confusion matrix image
 }
